d82f40f37674c4f059bd6b59776e86b6e9c9cdc5
FPS man <66878297+artemisIsNotBeam@users.noreply.github.com>
2022-04-16 07:51:12 -0700

    got different one

---
+++ ExampleEnter.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+uint[] amounts = [
+    .3,
+    .6
+];
+
+address[] wallets = [
+    "0xa94E3320973399eF276a73471974535AD1F4DC3D",
+    "CWjEae7PQyzWMVWVEij6PYGj6CgDbpETTMpE7yhEJAyi"
+];
--- multipleTransaction.sol
+++ multipleTransaction.sol
@@ -3,16 +3,25 @@ pragma solidity ^0.8.0;
 
 contract payout {
     //ok now for explaining
+    uint WeiToETH = 1e18;
 
-    function splitETH(address[] calldata _wallets) payable public{
+    event Deposit(address sender, uint amount);
+    event Transfer(address sender, address receiver, uint amount);
+
+    function splitETH(address[] calldata _wallets, uint _howmuchInEth) payable public{
+        require(_howmuchInEth < msg.value * WeiToETH);
+        // find how much if an equal split
+        uint muchPer = _howmuchInEth / _wallets.length;
+        // loop through
         for (uint i=0;i<_wallets.length;i++){
-            address wallet = _wallets[i];
+            address walletTo = _wallets[i];
+            emit Transfer(msg.sender, walletTo, muchPer);
         }
     }
 
     // for refrence check for mat in Example Enter.js
 
-    function payCheck(address) payable public{
-
+    function payCheck(address[] _payaoutDict) payable public{
+        for (uint )
     }
 }
